4f85ca96ee366d832ac6e9a75b10cda50ff8da40
Wad7 <vadim-mikheev777@inbox.ru>
2023-09-03 15:29:30 +0300

    Add commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
- import { createRoot } from 'react-dom/client';
- import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
- it('renders without crashing', () => {
-   const div = document.createElement('div');
-   const root = createRoot(div);
-   root.render(<App />);
-<<<<<<< HEAD
- });
-=======
- });
->>>>>>> 62a913c (Initial commit)
+it('отрисовывает компонент без ошибок', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App />);
+});
+
